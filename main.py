16deb8cdd0e83017d84ea60a9a28101f9e5c10b6
Jason Wu <jasonwupilly1997@gmail.com>
2016-01-30 13:41:29 -0500

    timer added

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import time
+
 import network as net
 import parser as parse
+
+TICK_TIME = 0.5
 
 bc = net.BClient.bclient_default()
 companies = None
@@ -7,3 +11,13 @@ def init_tickers():
     return parse.parse_securities(bc.cmd("SECURITIES")[0])
 
 companies = init_tickers()
+print(companies)
+
+st = time.time()
+
+while True:
+    if time.time() - st >= TICK_TIME:
+        st = time.time()
+        print("tick")
+    else:
+        continue
