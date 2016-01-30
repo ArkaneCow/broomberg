685b49e30b1ebc8e364e72498e435a13f56c8cc3
Shah, Rohil R <rohil@gatech.edu>
2016-01-30 15:02:06 -0500

    Update all

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def order_update(data):
     company = p['ASK']['ticker']
     companies[company]['price'] = p['ASK']['price']
     orders[company] = p
-    
+
 
 def security_update(data):
     p = parse.parse_securities(data)
@@ -30,19 +30,20 @@ def security_update(data):
             companies[k][v] = p[k][v]
     print(companies)
 
+def update_all():
+    commands = ["SECURITIES"] + ["ORDERS " + t for t in companies]
+    responses = bc.cmd(*commands)
+    order_responses = [o for o in responses if o.startswith("SECURITY_ORDERS_OUT")]
+    security_responses = [o for o in responses if o.startswith("SECURITIES_OUT")]
+    for r in security_responses:
+        security_update(r)
+    for r in order_responses:
+        order_update(r)
+
+update_all()
 while True:
     if time.time() - st >= TICK_TIME:
+        update_all()
         st = time.time()
-        print("tick")
-        commands = ["SECURITIES"] + ["ORDERS " + t for t in companies]
-        print(commands)
-        responses = bc.cmd(*commands)
-        order_responses = [o for o in responses if o.startswith("SECURITY_ORDERS_OUT")]
-        security_responses = [o for o in responses if o.startswith("SECURITIES_OUT")]
-        for r in security_responses:
-            security_update(r)
-        for r in order_responses:
-            order_update(r)
-        
     else:
         continue
