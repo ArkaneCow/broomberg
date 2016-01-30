9b9e5b23a7349e5f9033fad958787a6688a37b1b
Jason Wu <jasonwupilly1997@gmail.com>
2016-01-30 14:44:04 -0500

    updates all values

--- main.py
+++ main.py
@@ -11,13 +11,38 @@ def init_tickers():
     return parse.parse_securities(bc.cmd("SECURITIES")[0])
 
 companies = init_tickers()
+orders = {}
 print(companies)
 
 st = time.time()
+
+def order_update(data):
+    p = parse.parse_orders(data)
+    company = p['ASK']['ticker']
+    companies[company]['price'] = p['ASK']['price']
+    orders[company] = p
+    
+
+def security_update(data):
+    p = parse.parse_securities(data)
+    for k in p:
+        for v in p[k]:
+            companies[k][v] = p[k][v]
+    print(companies)
 
 while True:
     if time.time() - st >= TICK_TIME:
         st = time.time()
         print("tick")
+        commands = ["SECURITIES"] + ["ORDERS " + t for t in companies]
+        print(commands)
+        responses = bc.cmd(*commands)
+        order_responses = [o for o in responses if o.startswith("SECURITY_ORDERS_OUT")]
+        security_responses = [o for o in responses if o.startswith("SECURITIES_OUT")]
+        for r in security_responses:
+            security_update(r)
+        for r in order_responses:
+            order_update(r)
+        
     else:
         continue
